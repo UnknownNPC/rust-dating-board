7e09ec00ed14236eb14ae1e8486f6dfa2380b9b2
Vitalii Zymukha <vzymu@googlecloud.corp-partner.google.com>
2023-02-23 18:20:19 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+mod database;
+
+use std::sync::Mutex;
+
+
+use sailfish::TemplateOnce;
+
+
+#[derive(TemplateOnce)]
+#[template(path = "home.stpl")]
+struct Home<'a> {
+  head_title: &'a str
+}
+
+async fn homepage() -> impl Responder {
+  HttpResponse::Ok().body(Home {head_title: "hello"}.render_once().unwrap())
+}
+
+
+#[actix_web::main]
+async fn main() {
+  let addr = "localhost:8080";
+  // let db = web::Data::new(Mutex::new(Db::new()));  
+  let server = HttpServer::new(move || {
+    App::new()
+      // .app_data(db.clone())
+      .route("/", web::get().to(homepage))
+  })
+  .bind(addr)
+  .unwrap()
+  .run();  println!("Server live at http://{}", addr);
+  server.await.unwrap();
+}
