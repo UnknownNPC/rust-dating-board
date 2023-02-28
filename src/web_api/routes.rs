adc9d2c2118bf4515e23fda88bbda518efb8c4c8
Vitalii Zymukha <vzymu@googlecloud.corp-partner.google.com>
2023-02-28 19:39:03 +0200

    refctor google sign in

--- src/web_api/routes.rs
+++ src/web_api/routes.rs
@@ -1,6 +1,10 @@
 use std::error::Error;
 
-use actix_web::{cookie::Cookie, web, HttpRequest, HttpResponse, Responder};
+use actix_web::{
+    cookie::Cookie,
+    http::{header, StatusCode},
+    web, HttpRequest, HttpResponse, Responder,
+};
 
 use sailfish::TemplateOnce;
 use serde::Deserialize;
@@ -20,14 +24,34 @@ pub struct OAuthCallback {
 struct Home<'a> {
     head_title: &'a str,
     error_msg: &'a str,
+    is_authorized: bool,
 }
 
 pub async fn homepage(auth_gate: AuthenticationGate) -> impl Responder {
+
+    fn get_homepage_html_body(
+        title: &str,
+        error_msg: Option<&str>,
+        is_authorized: bool,
+    ) -> HttpResponse {
+        let html = HttpResponse::Ok().body(
+            Home {
+                head_title: title,
+                error_msg: error_msg.unwrap_or(""),
+                is_authorized,
+            }
+            .render_once()
+            .unwrap(),
+        );
+    
+        html
+    }
+
     println!(
         "Inside the homepage endpoint. User auth status {}",
         auth_gate.is_authorized
     );
-    get_homepage_html_body("OK Page", None, None)
+    get_homepage_html_body("OK Page", None, auth_gate.is_authorized)
 }
 
 pub async fn google_sign_in(
@@ -36,19 +60,18 @@ pub async fn google_sign_in(
     callback_payload: web::Form<OAuthCallback>,
     request: HttpRequest,
 ) -> impl Responder {
-    if callback_payload.credential.is_empty() {
-        return get_homepage_html_body("error", Some("credential field is empty"), None);
-    }
-    if callback_payload.g_csrf_token.is_empty() {
-        return get_homepage_html_body("error", Some("g_csrf_token field is empty"), None);
-    }
+    fn redirect_to_home_page(jwt_cookie: Option<Cookie>, error: Option<&str>) -> HttpResponse {
+        let mut response_builder = HttpResponse::build(StatusCode::FOUND);
 
-    if Some(callback_payload.g_csrf_token.clone())
-        != request
-            .cookie("g_csrf_token")
-            .map(|f| f.value().to_string())
-    {
-        return get_homepage_html_body("error", Some("sign in security attack"), None);
+        if error.is_some() {
+            response_builder.append_header((header::LOCATION, format!("/?{}=true", error.unwrap())))
+        } else {
+            response_builder.append_header((header::LOCATION, "/"))
+        };
+        if jwt_cookie.is_some() {
+            response_builder.cookie(jwt_cookie.unwrap());
+        };
+        response_builder.finish()
     }
 
     async fn fetch_and_save_user(
@@ -73,38 +96,32 @@ pub async fn google_sign_in(
         Ok(user)
     }
 
+    if callback_payload.credential.is_empty() {
+        return redirect_to_home_page(None, Some("lost_credential"));
+    }
+    if callback_payload.g_csrf_token.is_empty() {
+        return redirect_to_home_page(None, Some("lost_g_csrf_token"));
+    }
+
+    if Some(callback_payload.g_csrf_token.clone())
+        != request
+            .cookie("g_csrf_token")
+            .map(|f| f.value().to_string())
+    {
+        return redirect_to_home_page(None, Some("invalid_g_csrf_token"));
+    }
+
     let user_res = fetch_and_save_user(&db_provider, &callback_payload, &config).await;
 
     match user_res {
         Ok(user) => {
             let session_manager = AuthSessionManager::new(&config);
             let jwt_cookie = session_manager.get_jwt_token(user.id).await;
-
-            get_homepage_html_body(&format!("OK Page {:?}", user), None, Some(jwt_cookie))
+            redirect_to_home_page(Some(jwt_cookie), None)
         }
-        Err(err) => get_homepage_html_body(
-            "Error",
-            Some(&format!("Failed to retrive user {}", err.to_string())),
-            None,
-        ),
+        Err(err) => {
+            println!("Error happened during user fetch: {}", err);
+            redirect_to_home_page(None, Some("invalid_user"))
+        }
     }
-}
-
-fn get_homepage_html_body(
-    title: &str,
-    error_msg: Option<&str>,
-    jwt_cookie_opt: Option<Cookie>,
-) -> HttpResponse {
-    let mut html = HttpResponse::Ok().body(
-        Home {
-            head_title: title,
-            error_msg: error_msg.unwrap_or(""),
-        }
-        .render_once()
-        .unwrap(),
-    );
-
-    jwt_cookie_opt.map(|f| html.add_cookie(&f));
-
-    html
 }
