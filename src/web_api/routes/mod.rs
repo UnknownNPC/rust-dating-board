a3ab2f1824eed7dfc5250bc750e82129e97677f8
Vitalii Zymukha <vzymu@googlecloud.corp-partner.google.com>
2023-03-09 20:32:00 +0200

    refactor

--- src/web_api/routes/mod.rs
+++ src/web_api/routes/mod.rs
@@ -8,14 +8,17 @@ mod constants;
 mod profile_endpoints;
 
 
+
+pub use home_page::index_page;
+
 pub use add_profile_page::add_profile_get;
 pub use add_profile_page::add_profile_post;
 
-pub use profile_endpoints::delete_profile_photo_endpoint;
+pub use edit_profile_page::edit_profile_page;
+
 pub use profile_endpoints::add_profile_photo_endpoint;
+pub use profile_endpoints::delete_profile_photo_endpoint;
 pub use profile_endpoints::delete_profile_endpoint;
 
 pub use authorization_endpoint::google_sign_in_endpoint;
 pub use authorization_endpoint::sign_out_endpoint;
-pub use home_page::index_page;
-pub use edit_profile_page::edit_profile_page;
